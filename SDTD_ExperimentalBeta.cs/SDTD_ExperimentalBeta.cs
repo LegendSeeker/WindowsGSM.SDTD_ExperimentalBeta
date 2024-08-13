2028d284d2a301076f74cacfdcecab051ce73c66
raziel7893 <raziel7893@gmail.com>
2024-08-13 13:23:39 +0200

    fix message when save is in old location

--- SDTD_ExperimentalBeta.cs/SDTD_ExperimentalBeta.cs
+++ SDTD_ExperimentalBeta.cs/SDTD_ExperimentalBeta.cs
@@ -142,8 +142,10 @@ namespace WindowsGSM.Plugins
             if (Directory.Exists(OldProfile))
             {
                 Notice = Notice + $"An Old Savegame found in {OldProfile}\n\n" +
-                        $"If you want to use it, you need to move/copy it manually to \n" +
+                        $"If you want to have it Inside WindowsGSM to have it all in one place move it to\n" +
                         $"servers/{serverData.ServerID}/serverfiles/userdata.\n" +
+                        $"and modify value \"UserDataFolder\" in serverconfig.xml to value \"userdata\"\n" +
+                        $"For More info see the readme of this plugin\n" +
                         $"If this is intentional, ignore this";
             }
 
